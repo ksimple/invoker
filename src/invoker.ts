5bfbf30781f45f55b6bba480df728dbb783e63e0
Bin Du <bindu@ksimple.org>
2014-09-18 09:49:02 +0800

    Change calling spec

--- src/invoker.ts
+++ src/invoker.ts
@@ -1,86 +1,91 @@
-class invoker {
-    private context: any;
+var $invoke = (function() {
+    function createInvoke() {
+        var injection = {};
+        var config = {};
 
-    public constructor() {
-        this.context = {};
-        this.context.$invoker = this;
-    }
-
-    public declareFactory(name, value) {
-        this.context[name] = {
-            type: 'factory',
-            raw: value
-        };
-    }
-
-    public declare(name, value) {
-        if (typeof value === 'function') {
-            this.context[name] = {
-                type: 'factory',
-                factory: value
-            };
-        } else {
-            this.context[name] = {
-                type: 'raw',
-                raw: value
-            };
+        function extraParameter(func) {
+            return [];
         }
-    }
 
-    private extraParameter(func) {
-        return [];
-    }
+        function invokeInternal(args, index, func, promise) {
+            while (1) {
+                if (index >= args.length) {
+                    // call promise
+                    func.apply(null, args);
+                    return;
+                }
 
-    private internalInvoke(args, index, func, promise) {
-        while (1) {
-            if (index >= args.length) {
-                // call promise
-                func.apply(null, args);
+                var oneInjection = injection[args];
+
+                if (!oneInjection) {
+                    args[index++] = undefined;
+                    continue;
+                }
+
+                if (oneInjection.type === 'raw') {
+                    args[index++] = oneInjection.raw;
+                    continue;
+                }
+
+                args[index++] = undefined;
+            }
+        }
+
+        $invoke = function $invoke(...args) {
+            if (args.length == 0) {
                 return;
             }
 
-            var config = this.context[args];
+            args = Array.prototype.slice.apply(arguments);
+            var func = args.pop();
 
-            if (!config) {
-                args[index++] = undefined;
-                continue;
+            if (typeof func === 'function' && args.length === 0) {
+                args = extraParameter(func);
+            } else if (typeof func === 'object' && func instanceof Array) {
+                args = func;
+                func = args.pop();
             }
 
-            if (config.type === 'raw') {
-                args[index++] = config.raw;
-                continue;
+            invokeInternal(args, 0, func, null);
+        };
+
+        $invoke.inject = function $invoke$inject(name, value) {
+            if (!this.injection) {
+                this.injection = {};
+            }
+            if (typeof value === 'function') {
+                injection[name] = {
+                    type: 'factory',
+                    factory: value
+                };
+            } else {
+                injection[name] = {
+                    type: 'raw',
+                    raw: value
+                };
+            }
+        };
+
+        $invoke.config = function $invoke$config(newConfig) {
+            for (var i in newConfig) {
+                config[i] = newConfig[i];
+            }
+        };
+
+        $invoke.clone = function $invoke$clone() {
+            var instance = createInvoke();
+
+            for (var i in injection) {
+                instance.inject(injection[i], injection[i]);
             }
 
-            args[index++] = undefined;
-        }
+            instance.config(config);
+            return instance;
+        };
+
+        return $invoke;
     }
 
-    public invoke() {
-        if (arguments.length == 0) {
-            return;
-        }
-
-        var args = Array.prototype.slice.apply(arguments);
-        var func = args.pop();
-
-        if (typeof func === 'function' && args.length === 0) {
-            args = this.extraParameter(func);
-        } else if (typeof func === 'object' && func instanceof Array) {
-            args = func;
-            func = args.pop();
-        }
-
-        this.internalInvoke(args, 0, func, null);
-    }
-
-    public clone() {
-        var instance = new invoker();
-
-        for (var i in this.context) {
-            instance.context[i] = this.context[i];
-        }
-
-        return instance;
-    }
-}
+    return createInvoke();
+})();
 
--- test/unit.spec.js
+++ test/unit.spec.js
@@ -1,50 +1,44 @@
 describe('unit', function() {
     it('create', function() {
-        var $invoker = new invoker();
-
-        expect(typeof $invoker).toBe('object');
+        expect(typeof $invoke).toBe('function');
     });
 
     it('invoke without arguments', function() {
-        var $invoker = new invoker();
         var called = false;
         var args = null;
         var test = function() { called = true; args = arguments; };
 
-        $invoker.invoke(test);
+        $invoke(test);
         expect(called).toBe(true);
     });
 
     it('invoke without arguments in array', function() {
-        var $invoker = new invoker();
         var called = false;
         var args = null;
         var test = function() { called = true; args = arguments; };
 
-        $invoker.invoke([test]);
+        $invoke([test]);
         expect(called).toBe(true);
     });
 
     it('invoke with raw value', function() {
-        var $invoker = new invoker();
         var called = false;
         var args = null;
         var test = function() { called = true; args = arguments; };
 
-        $invoker.declare('test', 1);
-        $invoker.invoke('test', test);
+        $invoke.inject('test', 1);
+        $invoke('test', test);
         expect(called).toBe(true);
         expect(args[0]).toBe(1);
     });
 
     it('invoke with raw value in array', function() {
-        var $invoker = new invoker();
         var called = false;
         var args = null;
         var test = function() { called = true; args = arguments; };
 
-        $invoker.declare('test', 1);
-        $invoker.invoke(['test', test]);
+        $invoke.inject('test', 1);
+        $invoke(['test', test]);
         expect(called).toBe(true);
         expect(args[0]).toBe(1);
     });
